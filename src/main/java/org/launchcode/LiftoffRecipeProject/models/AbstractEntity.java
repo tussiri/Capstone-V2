73350d3d5e32b8052feb0ae2a9e170aae0aa2c1d
Dhutchen001 <117687122+Dhutchen001@users.noreply.github.com>
2023-04-19 14:57:22 -0500

    created AbstractEntity and coded

---
+++ src/main/java/org/launchcode/LiftoffRecipeProject/models/AbstractEntity.java
@@ -0,0 +1,46 @@
+package org.launchcode.LiftoffRecipeProject.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
