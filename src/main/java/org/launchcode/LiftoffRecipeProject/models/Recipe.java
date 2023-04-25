ad17ceafa9c4998a3a52c259942a2a6f2ff198aa
Dhutchen001 <117687122+Dhutchen001@users.noreply.github.com>
2023-04-24 20:37:25 -0500

    Updated Recipe class with other perameters

--- src/main/java/org/launchcode/LiftoffRecipeProject/models/Recipe.java
+++ src/main/java/org/launchcode/LiftoffRecipeProject/models/Recipe.java
@@ -8,16 +8,64 @@ public class Recipe extends AbstractEntity {
     private String name;
 
     private List<String> ingredients;
-
     private String directions;
-
     private int time;
+    private Boolean favorite;
+    private String description;
+    private String category;
+    private String picture;
+    private List<String> allergens;
 
-    public Recipe(String name, List<String> ingredients, String directions, int time) {
+    public Recipe(String name, List<String> ingredients, String directions, int time, Boolean favorite, String description, String category, String picture, List<String> allergens) {
         this.name = name;
         this.ingredients = ingredients;
         this.directions = directions;
         this.time = time;
+        this.favorite = favorite;
+        this.description = description;
+        this.category = category;
+        this.picture = picture;
+        this.allergens = allergens;
+    }
+
+    public Boolean getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public List<String> getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(List<String> allergens) {
+        this.allergens = allergens;
     }
 
     public String getName() {
