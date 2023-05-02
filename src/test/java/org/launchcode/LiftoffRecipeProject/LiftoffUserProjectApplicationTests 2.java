75478c52e79e996381d7fced06fa046143e11c43
Tumaini Ussiri <40671546+tussiri@users.noreply.github.com>
2023-05-01 20:15:58 -0500

    RecipeController, UserController completed.

---
+++ src/test/java/org/launchcode/LiftoffRecipeProject/LiftoffUserProjectApplicationTests 2.java
@@ -0,0 +1,13 @@
+package org.launchcode.LiftoffRecipeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffUserProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
