8f435cfe2fd2a42caca52760284b4376938f9266
Anjana Asokan <anjanaasokan@Anjanas-MacBook-Air.local>
2023-05-13 22:15:08 +0530

    Added photo upload option in New Recipe Page and created Update Recipe Page

--- liftoff-recipe-app/src/App.js
+++ liftoff-recipe-app/src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import HomePage from "./Pages/HomePage";
 import SignUp from "./Pages/SignUp";
 import NewRecipePage from "./Pages/NewRecipePage";
+import UpdateRecipePage from "./Pages/UpdateRecipePage";
 
 function App() {
   return (
     <div className="App">
-      <NewRecipePage />
+      <UpdateRecipePage />
     </div>
   );
 }
