ff4cc4eaeec8ba46c2c3f4b011b9506521ef70b1
Dhutchen001 <117687122+Dhutchen001@users.noreply.github.com>
2023-05-17 14:41:55 -0500

    Added React Routing and did a few style tweaks on home page

--- liftoff-recipe-app/src/Pages/HomePage.jsx
+++ liftoff-recipe-app/src/Pages/HomePage.jsx
@@ -7,12 +7,19 @@ import ChocoPic from "../Assets/Rectangle 68.png"
 import TortaPic from "../Assets/Rectangle 69.png"
 import TirraPic from "../Assets/Rectangle 70.png"
 import NavBar from "../Components/NavBar"
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 function HomePage() {
 return(
     <>
     <NavBar />
-
+    <Link to="login"><Button variant="contained">Log In</Button></Link>
+    <span>&nbsp;&nbsp;&nbsp;</span>
+    <Link to="signup"><Button variant="contained">Sign Up</Button></Link>
+    <p></p>
+    <Link to="newrecipe"><Button variant="contained">New Recipe</Button></Link>
+    <p>~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>
     <FoodCard title='Chicken' time='30 min' image={ChickenPic}></FoodCard>
     <FoodCard title='Beef Pasta' time='20 min' image={PastaPic}></FoodCard>
     <FoodCard title='Birria Tacos' time='25 min' image={TacoPic}></FoodCard>
