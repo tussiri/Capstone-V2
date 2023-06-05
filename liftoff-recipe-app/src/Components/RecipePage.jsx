97c74594c4945ca9b33f6d9ebf869387cb8dedaf
Tumaini Ussiri <40671546+tussiri@users.noreply.github.com>
2023-06-05 18:50:39 -0500

    worked on search functionality, frontend connections with the front end, etch

--- liftoff-recipe-app/src/Components/RecipePage.jsx
+++ liftoff-recipe-app/src/Components/RecipePage.jsx
@@ -1,26 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {Link, useParams} from 'react-router-dom'
+import authAxios from "../utility/authAxios";
 
-function RecipePage({ match }) {
+function RecipePage({match}) {
     const [recipe, setRecipe] = useState(null);
-    const {id} = useParams();
+    const [reviews, setReviews] = useState([]);
+    const {recipeId, userId} = useParams();
 
     useEffect(() => {
-        const token=localStorage.getItem('token');
-        const config={
-            headers:{
-                Authorization:`Bearer ${token}`
-            }
-        }
-        axios
-            .get(`http://localhost:8080/recipes/${id}`, config)
+        // const token = localStorage.getItem('token');
+        // const userId = localStorage.getItem('userId')
+        console.log(recipeId);
+        authAxios
+            .get(`http://localhost:8080/recipes/${recipeId}`)
             .then((response) => {
                 setRecipe(response.data.data);
-                console.log(response.data)
+                console.log(response.data.data)
             })
             .catch((error) => console.error(error));
-    }, [id]);
+
+        axios
+            .get(`http://localhost:8080/review/recipe/${recipeId}`)
+            .then((response) => {
+                setReviews(response.data.slice(0, 5));
+            })
+            .catch((error) => console.error(error));
+    }, [recipeId]);
 
     if (!recipe) {
         return <div>Loading...</div>;
@@ -29,14 +35,28 @@ function RecipePage({ match }) {
     return (
         <div>
             <h2>{recipe.name}</h2>
-            <img src={recipe.picture} alt = {recipe.name} />
+            <img src={recipe.picture} alt={recipe.name}/>
             <p>{recipe.description}</p>
             <p>Category: {recipe.category}</p>
             <p>Preparation time: {recipe.time} minutes</p>
             <p>Ingredients: {recipe.ingredients.join(", ")}</p>
             <p>Directions: {recipe.directions}</p>
             <p>Allergens: {recipe.allergens.join(", ")}</p>
-            <p>Rating: {recipe.ratings || <Link to={`/recipes/${recipe.id}/review`}>"Be the first to review this recipe!"</Link>}</p>
+
+            <h3>Reviews:</h3>
+            {reviews.length > 0 ? (
+                reviews.map((review) => (
+                    <div key={review.id}>
+                        <p>Rating: {review.rating}</p>
+                        <p>{review.comment}</p>
+                    </div>
+                ))
+            ) : (
+                <p>No reviews yet. Be the first!</p>
+            )}
+
+            <Link
+                to={`/recipes/${recipe.id}/review`}>{reviews.length > 0 ? "Leave your review" : "Be the first to review this recipe!"}</Link>
         </div>
     );
 }
--- liftoff-recipe-app/src/Pages/Dashboard.jsx
+++ liftoff-recipe-app/src/Pages/Dashboard.jsx
@@ -1,74 +1,9 @@
-// import React, { useState, useEffect } from 'react';
-// // import axios from 'axios';
-// //
-// // function Dashboard() {
-// //     // const [userRecipes, setUserRecipes] = useState([]);
-// //     // const [likedRecipes, setLikedRecipes] = useState([]);
-// //     // const [recommendedRecipes, setRecommendedRecipes] = useState([]);
-// //
-// //     useEffect(() => {
-// //         const token = localStorage.getItem('token');
-// //         const config = {
-// //             headers: { Authorization: `Bearer ${token}` }
-// //         };
-// //
-// //         axios.get('http://localhost:8080/user-recipes', config)
-// //             .then(response => {
-// //                 setUserRecipes(response.data);
-// //             });
-// //
-// //         axios.get('http://localhost:8080/liked-recipes', config)
-// //             .then(response => {
-// //                 setLikedRecipes(response.data);
-// //             });
-// //
-// //         // You would replace 'your-api-key' with your actual Spoonacular API key
-// //         axios.get('https://api.spoonacular.com/recipes/random?number=5&apiKey=a6f8abac7b0b4d4b9041bef47b8d82c2')
-// //             .then(response => {
-// //                 setRecommendedRecipes(response.data.recipes);
-// //             });
-// //     }, []);
-// //
-// //     return (
-// //         <div>
-// //             <h1>Your Recipes</h1>
-// //             {userRecipes.map(recipe => (
-// //                 <div key={recipe.id}>
-// //                     <h2>{recipe.title}</h2>
-// //                     <p>{recipe.description}</p>
-// //                     {/* Add other recipe details */}
-// //                 </div>
-// //             ))}
-// //
-// //             <h1>Liked Recipes</h1>
-// //             {likedRecipes.map(recipe => (
-// //                 <div key={recipe.id}>
-// //                     <h2>{recipe.title}</h2>
-// //                     <p>{recipe.description}</p>
-// //                     {/* Add other recipe details */}
-// //                 </div>
-// //             ))}
-// //
-// //             <h1>Recommended Recipes</h1>
-// //             {recommendedRecipes.map(recipe => (
-// //                 <div key={recipe.id}>
-// //                     <h2>{recipe.title}</h2>
-// //                     <p>{recipe.summary}</p>
-// //                     <img src={recipe.image} alt={recipe.title} />
-// //                     {/* Add other recipe details */}
-// //                 </div>
-// //             ))}
-// //         </div>
-// //     );
-// // }
-// //
-// // export default Dashboard;
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import FoodCard from "../Components/FoodCard";
 import "../Styles/Dashboard.css"
+import authAxios from "../utility/authAxios";
 
 function Dashboard() {
     const [recipes, setRecipes] = useState([]);
@@ -76,16 +11,12 @@ function Dashboard() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const token = localStorage.getItem("token");
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        };
-        axios
-            .get("http://localhost:8080/recipes?page=0&size=5", config)
+
+        authAxios
+            .get("http://localhost:8080/recipes?page=0&size=8")
             .then((response) => {
                 const data = response.data.data;
+                console.log(response.data)
                 setRecipes(data.content);
             })
             .catch((error) => console.error(error));
@@ -102,8 +33,8 @@ function Dashboard() {
             .catch((error) => console.error(error));
     }, []);
 
-    const handleCardClick = (id) => {
-        navigate(`/recipes/${id}`);
+    const handleCardClick = (recipeId) => {
+        navigate(`/recipes/${recipeId}`);
     };
 
     return (
