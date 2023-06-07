4a358c5869bb426e45d847c66a45acffd808b848
Xavier <blockerxavier@gmail.com>
2023-06-07 07:26:33 -0500

    added delete recipe

--- liftoff-recipe-app/src/App.js
+++ liftoff-recipe-app/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./Pages/HomePage";
 import SignUp from "./Pages/SignUp";
 import NewRecipePage from "./Pages/NewRecipePage";
 import UpdateRecipePage from "./Pages/UpdateRecipePage";
+import DeleteRecipePage from "./Pages/DeleteRecipePage";
 import Login from "./Pages/Login"
 import AccountInfo from "./Pages/AccountInfo"
 import Dashboard from "./Pages/Dashboard";
@@ -24,6 +25,7 @@ function App() {
                     <Route path="/signup" element={<SignUp/>}/>
                     <Route path="/newrecipe" element={<NewRecipePage/>}/>
                     <Route path="/updaterecipe" element={<UpdateRecipePage/>}/>
+                    <Route path="/deleterecipe" element={ <DeleteRecipePage/> } />
                     <Route path="/recipes/:recipeId" element={<RecipePage/>}/>
                     <Route path="/recipes/:recipeId/review" element={<ReviewPage/>}/>
                     <Route path="/review/:recipeId" component={ReviewPage}/>
