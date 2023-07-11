eb5c6192818ff0f771ead2500632732a4f2a1a29
Tuma <tussiri@gmail.com>
2023-07-11 14:09:20 -0500

    changed theme, worked on favorite functionality

--- backend/src/test/java/org/launchcode/LiftoffRecipeProject/FavoriteRecipeServiceTest.java
+++ backend/src/test/java/org/launchcode/LiftoffRecipeProject/FavoriteRecipeServiceTest.java
@@ -1,2 +1,39 @@
-package org.launchcode.LiftoffRecipeProject;public class FavoriteRecipeServiceTest {
+package org.launchcode.LiftoffRecipeProject;
+
+import org.junit.jupiter.api.Test;
+import org.launchcode.LiftoffRecipeProject.data.FavoriteRepository;
+import org.launchcode.LiftoffRecipeProject.services.FavoriteRecipeService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.launchcode.LiftoffRecipeProject.models.Favorite;
+import org.launchcode.LiftoffRecipeProject.models.User;
+import org.launchcode.LiftoffRecipeProject.models.Recipe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class FavoriteRecipeServiceTest {
+
+    @InjectMocks
+    FavoriteRecipeService favoriteRecipeService;
+
+    @Mock
+    FavoriteRepository favoriteRepository;
+
+    @Test
+    public void testAddFavorite() {
+        Favorite favorite = new Favorite();
+        favorite.setId(1);
+        favorite.setUser(new User());
+        favorite.setRecipe(new Recipe());
+
+        when(favoriteRepository.save(any(Favorite.class))).thenReturn(favorite);
+
+        Favorite result = favoriteRecipeService.addFavorite(favorite);
+
+        assertEquals(favorite.getId(), result.getId());
+    }
 }
