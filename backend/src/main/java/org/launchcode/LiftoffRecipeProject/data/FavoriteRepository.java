2b6caaa9190e9119cd6ddfa0fe035a14f01ba59c
Tuma <tussiri@gmail.com>
2023-07-11 14:09:20 -0500

    changed theme, worked on favorite functionality

--- backend/src/main/java/org/launchcode/LiftoffRecipeProject/data/FavoriteRepository.java
+++ backend/src/main/java/org/launchcode/LiftoffRecipeProject/data/FavoriteRepository.java
@@ -1,2 +1,19 @@
-package org.launchcode.LiftoffRecipeProject.data;public class FavoriteRepository {
+package org.launchcode.LiftoffRecipeProject.data;
+
+import org.launchcode.LiftoffRecipeProject.models.Favorite;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FavoriteRepository extends CrudRepository<Favorite, Integer> {
+    List<Favorite> findByUserId(Integer userId);
+//    Favorite findByUserIdAndRecipeId(Integer userId, Integer recipeId);
+
+    Optional<Object> findByUserAndRecipe(Integer user, Integer recipe);
+
+    Optional<Favorite> findByUserIdAndRecipeId(Integer userId, Integer recipeId);
+
 }
