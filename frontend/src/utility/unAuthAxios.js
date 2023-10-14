4bdd51845eb68d268630bf78eecbaf263a6452f2
Tuma Ussiri <tussiri@gmail.com>
2023-10-13 22:21:28 -0500

    fixing things

--- frontend/src/utility/unAuthAxios.js
+++ frontend/src/utility/unAuthAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const unAuthAxios = axios.create({
-    baseUrl:'http://localhost:8080/recipes/search?'
+    baseUrl:`${process.env.REACT_APP_BACKEND_URL}/recipes/search?`
 })
 export default unAuthAxios
